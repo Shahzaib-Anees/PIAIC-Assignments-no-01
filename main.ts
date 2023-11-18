bc7e6094cb2a3e8e74a960042d1e00761004029a
Shahzaib-Anees <aneesshahzaib27@gmail.com>
2023-11-18 18:55:32 +0500

    #modified

--- main.ts
+++ main.ts
@@ -1,2 +1,23 @@
-// Question no 01
-console.log("Hello! I am Shahzaib");
+// Question # 02
+let personMessage = "Steward Edison";
+let myMessage="Are you free today?"
+console.log(`Hello ${personMessage}!
+ ${myMessage} `)
+
+ //  Question # 03
+let a="Ayyan";
+// For LowerCase
+console.log(a.toLowerCase());
+// For Uppercase 
+console.log(a.toUpperCase());
+// for titlecase
+console.log(a);
+
+// Question no 04
+let aFamousPerson="Eleanor Rooseveit";
+console.log(`${aFamousPerson} says,"The Future belongs to those who believes in the beauty of their dreams."`);
+
+// Question no 05
+let FamousPerson = "'Thomas Jefferson' says;";
+let message="'I find that the hardest I work,the more luck I seems to have.'"
+console.log(`${FamousPerson} ${message}`); 
