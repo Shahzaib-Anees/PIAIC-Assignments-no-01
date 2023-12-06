c1674fc0adcf7288f8f4ac1e9a3d8f714ac8a714
Shahzaib-Anees <aneesshahzaib27@gmail.com>
2023-12-06 09:02:16 +0500

    #modified

---
+++ Qno10-20/Q no18-20/main.ts
@@ -0,0 +1,13 @@
+// Question no 18
+// Seeing the World: Think of at least five places in the world you’d like to visit. 
+let places:string[]=[
+    "Tower of Pisa","Burlin","Istanbul","Egyptian pyramids","Colossem"
+]
+// As the above array is not in alphabetic order
+console.log("Original Array",places);
+// For arranging array in alphabetic order 
+// console.log(places);
+//As the still in itd original state
+console.log(places);
+// For Printing Array in reverse Alphabetic order 
+console.log(places.reverse());
