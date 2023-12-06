563eb892d20900fb49d18e883147f2b5122bebbc
Shahzaib-Anees <aneesshahzaib27@gmail.com>
2023-12-06 09:15:37 +0500

    #modified

--- Qno10-20/Q no18-20/main.js
+++ Qno10-20/Q no18-20/main.js
@@ -7,8 +7,8 @@ let places = [
 // As the above array is not in alphabetic order
 console.log("Original Array", places);
 // For arranging array in alphabetic order 
-// console.log(places);
+console.log(places.sort());
 //As the still in itd original state
-console.log(places);
+// console.log(places);
 // For Printing Array in reverse Alphabetic order 
 console.log(places.reverse());
