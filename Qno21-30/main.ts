75dfe8a0ab8f18e893f4c562b2ebee5a23e243ac
Shahzaib-Anees <aneesshahzaib27@gmail.com>
2023-12-14 09:50:22 +0500

    #modified

--- Qno21-30/main.ts
+++ Qno21-30/main.ts
@@ -119,58 +119,97 @@ console.log(MyCartItems == "Rice")//Predict True
 // This means that rice are present in MyCartItems
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Question no 25
 // Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
-
-
+let alien_color = "Green";
 // • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if (alien_color == "Green") {
+    console.log("The player just earned 5 points.")
+} else {
+    console.log("");
+}
 // • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
-
+// The program that passes the If test 
+if (alien_color == "Green") {
+    console.log("true");
+} else {
+    console.log("False")
+};
+// The Program that fails the If test;
+if (alien_color == "Yellow") {
+    console.log("true")
+} else { };
 
 //Question no 26
 // Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
 
-// let AlienColor =  "Green";
+let AlienColor = "Green";
 // // • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
-// if (AlienColor === "Green") {
-//     console.log("The Player has just got 5 points for shooting the alien")
-// }
+if (AlienColor === "Green") {
+    console.log("The Player has just got 5 points for shooting the alien")
+}
 
 // • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+AlienColor = "Blue";
 
-// let AlienColor="blue";
-// if (AlienColor!="Green"){
-//     console.log(" The player just earned 10 points.")
-// }else{};
+if (AlienColor != "Green") {
+    console.log(" The player just earned 10 points.")
+} else { };
 
 // • Write one version of this program that runs the if block and another that runs the else block.
 // First Version
-
-// if (AlienColor == "Green") {
-//     console.log("The Player has just got 5 points for shooting the alien")
-// }else{
-//     console.log("");
-// }
+// Else Runner 
+if (AlienColor == "Green") {
+    console.log("The Player has just got 5 points for shooting the alien")
+} else {
+    console.log("Sorry! You are Failed");
+}
 
 // Second Version
-// if(AlienColor=="blue"){
-//     console.log("You have cleared this stage")
-// }else{
-//     console.log("Sorry! You are Failed")
-//}
+// If Runner 
+if (AlienColor == "Blue") {
+    console.log("You are corrct")
+} else {
+    console.log("Sorry! You are Failed")
+}
+
+// Quesyion no 27
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • If the alien is green, print a message that the player earned 5 points.
+
+
+// • If the alien is yellow, print a message that the player earned 10 points.
+
+// • If the alien is red, print a message that the player earned 15 points.
+
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+// Question no 28 
+// Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then
+// • If the person is less than 2 years old, print a message that the person is a baby.
+let PersonAge = 65;
+// • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+
+if (PersonAge < 2) {
+    console.log("The person is a baby")
+}
+// • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+else if (PersonAge == 2) {
+    console.log("The Person is a Kid")
+}
+
+// • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+else if (PersonAge == 13 || PersonAge < 20) {
+    console.log("The Person is a Teenager")
+}
+// • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+else if (PersonAge == 20 || PersonAge < 65) {
+    console.log("The Person is an adult")
+}
+// • If the person is age 65 or older, print a message that the person is an elder.
+else {
+    console.log("The Person is an Elder")
+}
 
 
 
