dffad9a5128a6cb65320238c2947c17f2165db7d
Shahzaib-Anees <aneesshahzaib27@gmail.com>
2023-12-16 17:01:38 +0500

    #modified

--- Qno21-30/main.js
+++ Qno21-30/main.js
@@ -21,6 +21,7 @@ let famousPlacesPakistan = [
 let validIndex = 7;
 let mostFamous = famousPlacesPakistan[validIndex];
 console.log(mostFamous);
+
 // question no 23
 // Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
 // let car = 'subaru';
@@ -28,6 +29,7 @@ console.log(mostFamous);
 // console.log(car == 'subaru')
 // • Look closely at your results, and make sure you understand why each line evaluates to True or False. 
 // • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False. 
+
 let aNumber = 19;
 let otherNumber = 21;
 console.log(aNumber == otherNumber); //predict false;
@@ -51,8 +53,8 @@ cityofAmerica = "Chicago";
 console.log(cityofAmerica == "London"); //predict false
 let GermanCity = "Hamsberdge";
 GermanCity = "Berlin";
-console.log(GermanCity != "Berlin"); //perdict false
-;
+console.log(GermanCity != "Berlin"); //perdict false;
+
 // Question no 24
 // More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
 // • Tests for equality and inequality with strings
@@ -84,6 +86,7 @@ console.log(MyCartItems == "Black Chilli Powder"); //Predict False
 // This means that the Black Chilli Powder is not Present in MyCartItems;
 console.log(MyCartItems == "Rice"); //Predict True
 // This means that rice are present in MyCartItems
+
 // Question no 25
 // Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
 let alien_color = "Green";
@@ -107,8 +110,8 @@ else {
 if (alien_color == "Yellow") {
     console.log("true");
 }
-else { }
-;
+else { };
+
 //Question no 26
 // Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
 let AlienColor = "Green";
@@ -135,27 +138,44 @@ else {
 // Second Version
 // If Runner 
 if (AlienColor == "Blue") {
-    console.log("You are corrct");
+    console.log("You are correct");
 }
 else {
     console.log("Sorry! You are Failed");
 }
+
 // Quesyion no 27
 // Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+AlienColor="Green";
 // • If the alien is green, print a message that the player earned 5 points.
+if(AlienColor=="Green"){
+    console.log("The player has earned 5 Points")
+}else{};
 // • If the alien is yellow, print a message that the player earned 10 points.
+AlienColor="Yellow";
+if(AlienColor=="Yellow"){
+    console.log("The player has earned 10 Points")
+}else{};
+
 // • If the alien is red, print a message that the player earned 15 points.
+AlienColor="Red";
+if(AlienColor=="Red"){
+    console.log("The player has earned 15 Points")
+}else{};
+
+// Question no 28
 // • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
 // Question no 28 
 // Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then
 // • If the person is less than 2 years old, print a message that the person is a baby.
-let PersonAge = 65;
+let PersonAge = 3;
 // • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
 if (PersonAge < 2) {
     console.log("The person is a baby");
 }
 // • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
-else if (PersonAge == 2) {
+else if (PersonAge == 4 || PersonAge<13) {
     console.log("The Person is a Kid");
 }
 // • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
@@ -170,6 +190,7 @@ else if (PersonAge == 20 || PersonAge < 65) {
 else {
     console.log("The Person is an Elder");
 }
+
 // Question no 29 
 // Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
 let FruitNames = [
