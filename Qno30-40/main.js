194bc97eb905eee32d546a7fe1a1868a7fbb8116
Shahzaib-Anees <aneesshahzaib27@gmail.com>
2023-12-16 20:59:28 +0500

    #modified

--- Qno30-40/main.js
+++ Qno30-40/main.js
@@ -62,7 +62,3 @@ for (let a = 0; a < 1; a++) {
 let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // • Loop through the array.
 // • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
-for (let i = 0; i < 10; i++) {
-    if ()
-        ;
-}
