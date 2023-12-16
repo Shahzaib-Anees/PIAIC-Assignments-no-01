aceed397c247ef978255e73f6ec424b8b2297f8d
Shahzaib-Anees <aneesshahzaib27@gmail.com>
2023-12-16 21:41:23 +0500

    #modified

--- Qno30-40/main.js
+++ Qno30-40/main.js
@@ -59,6 +59,34 @@ for (let a = 0; a < 1; a++) {
 // Question no 33 
 // Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
 // • Store the numbers 1 through 9 in a array.
-let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // • Loop through the array.
 // • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
+for (let nums of num) {
+    let ordinal;
+    if (nums === 1) {
+        ordinal = "st";
+    }
+    else if (nums == 2) {
+        ordinal = "nd";
+    }
+    else if (nums == 3) {
+        ordinal = "rd";
+    }
+    else {
+        ordinal = "th";
+    }
+    console.log(`${nums}${ordinal}`);
+}
+// Question no 34
+// Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+let myFavouritePizzas = ["Turkish Kabab Pizza", "Tikka Faggita Pizza", "Cheese Pizza"];
+// • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+for (let i = 0; i < 3; i++) {
+    console.log(myFavouritePizzas[i]);
+}
+for (let i = 0; i < 3; i++) {
+    console.log(`I really like ${myFavouritePizzas[i]}`);
+}
+;
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
